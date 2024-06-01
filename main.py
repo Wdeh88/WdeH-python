e98db0c10aa2fc760de1915458118672b80fec44
Wdeh88 <150028596+Wdeh88@users.noreply.github.com>
2024-06-01 15:37:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+import datetime,time,webbrowser
+now = datetime.date.today();target = datetime.date(2024,12, 11)
+if now >=target:print('\x1b[38;5;208m⇼'*60);print('\x1b[38;5;220m ➔ \x1b[38;5;48m تـم ايـقـاف الاداة مـن قـبـل الـمـبـرمـج ᭙ძᥱɦ ᥲᥣᖇ᥆᥉ᥱ');print('\x1b[38;5;208m⇼'*60);time.sleep(3);print('\x1b[38;5;220m ➔ \x1b[38;5;48m @WDEH_ALROSE الـمـبـرمـج ');webbrowser.open('https://t.me/wdeh_alrose');time.sleep(3);print('\x1b[38;5;208m⇼'*60);print('\x1b[38;5;220m ➔ \x1b[38;5;48m @YRWSYY قـنـاة الـمـبـرمـج ');print('\x1b[38;5;208m⇼'*60);webbrowser.open('https://t.me/YRWSYY');time.sleep(1)
+else:print("\033[1;32mشغالـة✔️")
